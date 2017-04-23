4c197e02d968c32a7c43b407643166791eea1c6d
zdelilo <zdelilo@mix.wvu.edu>
2017-04-23 17:45:52 -0400

    added poll open restrictions

--- src/Election.java
+++ src/Election.java
@@ -1,30 +1,52 @@
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-public class Election  implements java.io.Serializable{
+public class Election implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	String election_title;
-	 
-	 HashMap<String, User> votedUsers;			/**UserName -> User**/
-	 HashMap<String, String[]> IDStatistics;    /**VoterID -> Statistics**/   
-	 HashMap<String, List<Candidate>> voterIDs; /**VoterIDs -> Candidates**/ 
-	 HashMap<String, List<Candidate>> votes;	/**Race -> Candidates**/
-	 HashMap<String, Integer> summaryStatistcs; /**UserName -> SummaryStatistics**/
-	
-	public Election(String title)
-	{
+	Date pollStartDay;
+	Date pollEndDay;
+
+	HashMap<String, User> votedUsers;
+	/** UserName -> User **/
+	HashMap<String, String[]> IDStatistics;
+	/** VoterID -> Statistics **/
+	HashMap<String, List<Candidate>> voterIDs;
+	/** VoterIDs -> Candidates **/
+	HashMap<String, List<Candidate>> votes;
+	/** Race -> Candidates **/
+	HashMap<String, Integer> summaryStatistcs;
+
+	/** UserName -> SummaryStatistics **/
+
+	public Election(String title, Date start, Date end) {
 		election_title = title;
-		votedUsers = new  HashMap<String, User>();
+		pollStartDay = start;
+		pollEndDay = end;
+		
+		votedUsers = new HashMap<String, User>();
 		votes = new HashMap<String, List<Candidate>>();
 		voterIDs = new HashMap<String, List<Candidate>>();
-		summaryStatistcs= new HashMap<String, Integer>();
+		summaryStatistcs = new HashMap<String, Integer>();
 		IDStatistics = new HashMap<String, String[]>();
+		
+	}
+
+	public Date getPollStart(){
+		return pollStartDay;
 	}
 	
-	public String toString()
-	{
-		return election_title;
+	public Date getPollEnd(){
+		return pollEndDay;
+		
+	}
+	
+	public String toString() {
+		
+		return election_title + " from " + pollStartDay + " to " + pollEndDay;
+	
 	}
 }
--- src/ElectionInterface.java
+++ src/ElectionInterface.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -20,199 +22,245 @@ import javax.swing.JPanel;
 import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
 
-public class ElectionInterface extends JFrame implements ActionListener{
+public class ElectionInterface extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	DefaultListModel<String> model = new DefaultListModel<String>();
 	JList<String> list = new JList<String>(model);
 	Election[] elections;
-	
-	/**Layout and ButtonGroup Components**/
-    JPanel panelMain = new JPanel();
-    JPanel pnlList = new JPanel();
-    GroupLayout layout = new GroupLayout(panelMain);
-    JButton confirm;
-    JButton exit; 
-	JLabel votedError;
-    User user;
-    
-    /**Server Stuff**/
+	Date today = new Date();
+
+	/** Layout and ButtonGroup Components **/
+	JPanel panelMain = new JPanel();
+	JPanel pnlList = new JPanel();
+	GroupLayout layout = new GroupLayout(panelMain);
+	JButton confirm;
+	JButton exit;
+	JLabel votedError, pollingError;
+	User user;
+
+	/** Server Stuff **/
 	ObjectInputStream brIn;
-	ObjectOutputStream pwOut;	
+	ObjectOutputStream pwOut;
 	Socket sock;
-    
-	ElectionInterface(User user){
+
+	ElectionInterface(User user) {
 		startServer();
 		this.user = user;
 		confirm = new JButton("Confirm");
-    	confirm.setActionCommand("confirm");
-    	confirm.addActionListener(this);
-    	
-    	exit = new JButton("Exit");
-    	exit.setActionCommand("exit");
-    	exit.addActionListener(this);
-    	 
-    	votedError = new JLabel();
+		confirm.setActionCommand("confirm");
+		confirm.addActionListener(this);
+
+		exit = new JButton("Exit");
+		exit.setActionCommand("exit");
+		exit.addActionListener(this);
+
+		votedError = new JLabel();
 		votedError.setText("Oops! Seems as if you've already voted!");
 		votedError.setIcon(MyImages.yellowFrown);
 		votedError.setVisible(false);
-    	
-    	list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
- 		list.setVisibleRowCount(4);
- 		list.setVisible(true);
- 		 
- 		pnlList.add(list);
- 		pnlList.add(confirm);
+		
+		pollingError = new JLabel();
+		pollingError.setIcon(MyImages.yellowFrown);
+		pollingError.setVisible(false);
+
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setVisibleRowCount(4);
+		list.setVisible(true);
+
+		pnlList.add(list);
+		pnlList.add(confirm);
 		pnlList.setAutoscrolls(true);
 		pnlList.setBorder(BorderFactory.createLineBorder(Color.WHITE));
 		pnlList.setLayout(new BoxLayout(pnlList, BoxLayout.Y_AXIS));
+
 		
 		getElections();
-		for(Election e: elections)
-		{
+		for (Election e : elections) {
 			this.addElection(e.election_title);
 		}
 		addConfirm();
-		
-		/**Default values for Main Panel | Color | Size | Icon | Title | **/
-        panelMain.setBackground(MyColors.deepBlue);
-        this.setSize(400,200);
-        this.setIconImage(MyImages.codeFather.getImage());
-        this.setTitle("MyVote");
-        this.getContentPane().add(panelMain);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.getRootPane().setDefaultButton(confirm);
-       
-        /**Centers GUI to center of screen**/
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
-        int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
-        setLocation(x, y);
-        setVisible(true);
+
+		/** Default values for Main Panel | Color | Size | Icon | Title | **/
+		panelMain.setBackground(MyColors.deepBlue);
+		this.setSize(400, 200);
+		this.setIconImage(MyImages.codeFather.getImage());
+		this.setTitle("MyVote");
+		this.getContentPane().add(panelMain);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		this.getRootPane().setDefaultButton(confirm);
+
+		/** Centers GUI to center of screen **/
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
+		int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
+		setLocation(x, y);
+		setVisible(true);
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		/**If user confirms Election selection | selected election is written to server    |
-		 * 									   | User is checked if voted                  |
-		 * 									   | User GUI brought up for selected Election |**/
-		if(e.getActionCommand().equals("confirm") && !list.isSelectionEmpty()){
+		/**
+		 * If user confirms Election selection | selected election is written to
+		 * server | | User is checked if voted | | User GUI brought up for
+		 * selected Election |
+		 **/
+		if (e.getActionCommand().equals("confirm") && !list.isSelectionEmpty()) {
 			String selectedE = list.getSelectedValue().toString();
+			Election selected = null;
+			
+			for(int i = 0; i < elections.length; i++){
+				if(selectedE.equals(elections[i].election_title)) selected = elections[i];
+				System.out.println(elections[i].pollStartDay);
+			}
+			
 			try {
 				pwOut.writeObject("<selectedElection>");
 				pwOut.writeObject(selectedE);
-				
-				if(!hasVoted(user.username))
-				{
-				user.UserGUI(user);
-				this.setVisible(false);
+
+				if (!hasVoted(user.username) && pollsOpen()) {
+					user.UserGUI(user);
+					this.setVisible(false);
 				}
-				
-				else
-				{/**Displays error message if user has voted**/
-					votedError.setVisible(true);
+				else {/** Displays error message if user has voted **/
+					votedError.setVisible(pollsOpen());
+					SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+					
+					pollingError.setText("Oops! The polls are not open! Polls are active for this election between " + 
+							sdf.format(getStart()) + " at " + "7:00am" + " and ends at midnight " + sdf.format(getEnd()));
+					
+					pollingError.setVisible(!pollsOpen());
 				}
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 		}
-		/**User selects exit button | program exits |**/
-		if(e.getActionCommand().equals("exit"))
+		/** User selects exit button | program exits | **/
+		if (e.getActionCommand().equals("exit"))
 			shutdown();
 	}
-	
+
 	/**
 	 * Creates GUI that lists all available elections
-	 * @param eName - name of election
+	 * 
+	 * @param eName
+	 *            - name of election
 	 */
-	public void addElection(String eName) {
-		model.addElement(eName);
+	
+	public boolean pollsOpen(){
 		
-		layout.setHorizontalGroup(layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-						.addComponent(pnlList))
-				);
-
-		layout.setVerticalGroup(layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-						.addComponent(pnlList))
-				);
+		return (getEnd().after(today) && getStart().before(today));
+		
 	}
 	
+	public Date getEnd(){
+		Date pickle = null;
+			
+			try {
+				pwOut.writeObject("<End>");
+				pickle = (Date) brIn.readObject();
+			} catch (Exception e) {
+				
+				System.err.println(e.getMessage());
+		
+			}
+			
+			return pickle;
+			
+		}
+	
+	public Date getStart(){
+	Date duck = null;
+		
+		try {
+			pwOut.writeObject("<Start>");
+			duck = (Date) brIn.readObject();
+		} catch (Exception e) {
+			
+			System.err.println(e.getMessage());
+	
+		}
+		
+		return duck;
+		
+	}
+	
+	
+	public void addElection(String eName) {
+		model.addElement(eName);
+
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER).addComponent(pnlList)));
+
+		layout.setVerticalGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(pnlList)));
+	}
+
 	/**
-	 * @param username - name of user
+	 * @param username
+	 *            - name of user
 	 * @return check of user has voted or not | true | false |
 	 */
-	public boolean hasVoted(String username){
-	boolean voted = true;
+	public boolean hasVoted(String username) {
+		boolean voted = true;
 		try {
 			pwOut.writeObject("<hasVoted>");
 			pwOut.writeObject(username);
-			//System.out.println((String)brIn.readObject());
-			voted = (boolean)brIn.readObject();
-		} catch (IOException | ClassNotFoundException e) 
-		{
+			// System.out.println((String)brIn.readObject());
+			voted = (boolean) brIn.readObject();
+		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		return voted;
 	}
-	
+
 	/**
 	 * Adds Confimation and Exit buttons to end of GUI
 	 */
-	public void addConfirm(){
+	public void addConfirm() {
 		layout.setHorizontalGroup(layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-						.addComponent(confirm))
-						.addComponent(exit)
-						.addComponent(votedError)
-				
-				);
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER).addComponent(confirm))
+				.addComponent(exit).addComponent(votedError)
+
+		);
 
 		layout.setVerticalGroup(layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-						.addComponent(confirm))
-						.addComponent(exit)
-						.addComponent(votedError)
-				);
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(confirm))
+				.addComponent(exit).addComponent(votedError));
 	}
-	
-	/**Retreves array of current Elections from Server**/
-	public void getElections(){
+
+	/** Retreves array of current Elections from Server **/
+	public void getElections() {
 		try {
 			pwOut.writeObject("<getElections>");
 			elections = (Election[]) brIn.readObject();
-		} catch (IOException | ClassNotFoundException e) 
-		{
+		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	/**Starts Server**/
-	public void startServer(){
-		 /**Initialzies Server**/
-		 try {
-				sock = new Socket("127.0.0.1",50000);
-				pwOut = new ObjectOutputStream(sock.getOutputStream());
-				brIn = new ObjectInputStream(sock.getInputStream());    	
-			
-		 	} catch (IOException e) 
-		 	{
-				e.printStackTrace();
-			}
-	 }
-   
-   /**Disconnects Socket and server 
-    * Shuts down system**/
-public void shutdown(){
-		   try {
-				pwOut.writeObject("<shutdown>");
-				System.exit(0);
-			
-		   } catch (IOException e1) 
-		   {				
-				e1.printStackTrace();
-			}
-	   }
-}
 
-	
+	/** Starts Server **/
+	public void startServer() {
+		/** Initialzies Server **/
+		try {
+			sock = new Socket("127.0.0.1", 50000);
+			pwOut = new ObjectOutputStream(sock.getOutputStream());
+			brIn = new ObjectInputStream(sock.getInputStream());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Disconnects Socket and server Shuts down system
+	 **/
+	public void shutdown() {
+		try {
+			pwOut.writeObject("<shutdown>");
+			System.exit(0);
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
